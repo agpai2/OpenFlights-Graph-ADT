acb2e2b5039d38cd9891932517ed990cf3dc64a7
Aniruddha Pai <agpai2@linux-a3.ews.illinois.edu>
2020-11-23 17:27:29 -0600

    Implemented a function to calculate distance

--- Graph.cpp
+++ Graph.cpp
@@ -1,26 +1,45 @@
 #include "Graph.h"
-
+#include <cmath>
 
 Graph::Graph() {
-    adjMatrix.resize(_MAX_INT_DIG);
+    adjMatrix.resize(INT32_MAX);
 }
 
 
 void Graph::addEdge(Airport source, Airport destination) {
-
     double euclideanDist = euclideanDistance(source, destination);
     adjMatrix[source.getId() - 1][destination.getId() - 1] = euclideanDist;
 }
 
 
-bool Graph::isAdjacent(Airport source, Airport destination) {
-
+bool Graph::areAdjacent(Airport source, Airport destination) {
     return adjMatrix[source.getId() - 1][destination.getId() - 1] != 0.0;
 
 }
 
 double Graph::euclideanDistance(Airport source, Airport destination) {
-    return 0.0;
+    // logic taken from https://www.geeksforgeeks.org/program-distance-two-points-earth/
+    long double one_deg = (M_PI) / 180;
+
+    double source_lat = one_deg * source.getLatitude();
+    double source_long = one_deg * source.getLongitude();
+    double dest_lat = one_deg * destination.getLatitude();
+    double dest_long = one_deg * destination.getLongitude();
+
+    // Haversine Formula
+    long double dlong = dest_long - source_long;
+    long double dlat = dest_lat - source_lat;
+
+    long double distance = pow(sin(dlat / 2), 2) + 
+        cos(source_lat) * cos(dest_lat) * pow(sin(dlong / 2), 2);
+
+    distance = 2 * asin(sqrt(distance));
+
+    // Radius of Earth = 6371 Kilometers or 3956 Miles
+    long double R = 6371;
+    distance = distance * R;
+
+    return distance;
 }
 
 
--- Graph.h
+++ Graph.h
@@ -3,21 +3,34 @@
 
 class Graph {
     public:
+        /**
+         * Inserts an edge between two vertices (Airports).
+         * 
+         * @param source - one vertex (Airport) the edge is connected to
+         * @param destination - the other vertex (Airport) the edge is connected to
+         */
         void addEdge(Airport source, Airport destination);
-        
+
+        /**
+         * Checks if the vertices (Airports) are adjacent.
+         * 
+         * @param source - one vertex (Airport) the edge is connected to
+         * @param destination - the other vertex (Airport) the edge is connected to
+         * 
+         * @return if airports are adjacent
+         */
+        bool areAdjacent(Airport source, Airport destination);
+
+    private:
+        /**
+         * Calculates the distance between two airports using its latitudes and longitudes.
+         * 
+         * @param source - one vertex (Airport) the edge is connected to
+         * @param destination - the other vertex (Airport) the edge is connected to
+         * 
+         * @return the distance between two airports in kilometers
+         */
         double euclideanDistance(Airport source, Airport destination);
 
-        
-        
-    private:
-
         vector<vector<double>> adjMatrix;
-        bool isAdjacent(Airport source, Airport destination);
-
-
-        // insert(chenObj, Madradobj) {
-        // adjMatrix[chenn][madrad] = 1;
-        // }
-
-            
 };
